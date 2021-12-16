3e0811023693b37fc403cf0334539df3cb766ab4
kaiser101 <kaiser101@gmail.com>
2021-12-16 14:49:45 +0530

    Add proper ws connection handling code

--- mqtt_react/src/App.js
+++ mqtt_react/src/App.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const ws = new WebSocket("ws://localhost:8080");
-
 const App = () => {
-    ws.onopen = (event) => ws.send("70e6cc3986807aa9044f9542aa13bf57bf3cdbe3");
-
     const [msg, setMesg] = useState("");
 
     useEffect(() => {
+        const ws = new WebSocket("ws://localhost:8080");
+
+        ws.onopen = (event) =>
+            ws.send("70e6cc3986807aa9044f9542aa13bf57bf3cdbe3");
+
         ws.onmessage = (event) => setMesg(event.data);
 
-        return ws.close();
+        return () => {
+            ws.close();
+        };
     }, []);
 
     return <div className="App">{msg}</div>;
