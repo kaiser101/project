1361b0a89f73e062daf566faa5e5f9f6b7532282
kaiser101 <kaiser101@gmail.com>
2021-12-17 11:41:13 +0530

    Add container element

--- react-progressbar/src/Torrents.jsx
+++ react-progressbar/src/Torrents.jsx
@@ -9,6 +9,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import LinearWithValueLabel from "./LinearWithValueLabel";
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Torrents = () => {
     const [torrents, setTorrents] = useState([]);
@@ -34,54 +35,56 @@ const Torrents = () => {
 
     return (
         <div>
-            <Box
-                component="form"
-                sx={{
-                    "& > :not(style)": { m: 1, width: "25ch" },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField
-                    id="standard-basic"
-                    label="Standard"
-                    variant="standard"
-                    inputRef={newTorrent}
-                />
-                <Button variant="outlined" onClick={() => addTorrent()}>
-                    Add Torrent
-                </Button>
-                <Button variant="outlined" onClick={() => removeTorrent()}>
-                    Remove Torrent
-                </Button>
-            </Box>
-            <List
-                sx={{
-                    width: "100%",
-                    maxWidth: 600,
-                    bgcolor: "background.paper",
-                }}
-            >
-                {_.map(torrents, (torrent) => (
-                    <ListItem key={torrent}>
-                        <LinearWithValueLabel torrent={torrent} />
-                        <ListItemText
-                            primary={
-                                <Fragment>
-                                    <Typography
-                                        sx={{ display: "inline" }}
-                                        component="span"
-                                        variant="body2"
-                                        color="text.primary"
-                                    >
-                                        {torrent.substring(0, 6)}
-                                    </Typography>
-                                </Fragment>
-                            }
-                        />
-                    </ListItem>
-                ))}
-            </List>
+            <Container>
+                <Box
+                    component="form"
+                    sx={{
+                        "& > :not(style)": { m: 1, width: "25ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="standard-basic"
+                        label="Standard"
+                        variant="standard"
+                        inputRef={newTorrent}
+                    />
+                    <Button variant="outlined" onClick={() => addTorrent()}>
+                        Add Torrent
+                    </Button>
+                    <Button variant="outlined" onClick={() => removeTorrent()}>
+                        Remove Torrent
+                    </Button>
+                </Box>
+                <List
+                    sx={{
+                        width: "100%",
+                        maxWidth: 600,
+                        bgcolor: "background.paper",
+                    }}
+                >
+                    {_.map(torrents, (torrent) => (
+                        <ListItem key={torrent}>
+                            <LinearWithValueLabel torrent={torrent} />
+                            <ListItemText
+                                primary={
+                                    <Fragment>
+                                        <Typography
+                                            sx={{ display: "inline" }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            {torrent.substring(0, 6)}
+                                        </Typography>
+                                    </Fragment>
+                                }
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </Container>
         </div>
     );
 };
